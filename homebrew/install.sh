2e6255ebdb5aeb28a5f25211917f2170fe89681c
Neal <neal@ineal.me>
2013-03-28 20:39:47 -0500

    made everything to my liking - first commit from me

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -6,14 +6,46 @@
 # using Homebrew.
 
 # Check for Homebrew
-if test ! $(which brew)
-then
-  echo "  x You should probably install Homebrew first:"
-  echo "    https://github.com/mxcl/homebrew/wiki/installation"
-  exit
+if test ! $(which brew); then
+	echo -p "Homebrew is not installed! Would you like to install it? (y/n) " -n 1
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+	fi
+	exit
 fi
 
-# Install homebrew packages
-brew install grc coreutils spark
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+# Install GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+echo "Don’t forget to add $(brew --prefix coreutils)/libexec/gnubin to \$PATH."
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+
+# Install more recent versions of some OS X tools
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/screen
+
+# Install everything else
+brew install ack
+brew install automake
+brew install autoconf
+brew install cmake
+brew install git
+brew install htop-osx
+brew install nmap
+brew install rename
+brew install wget
+brew install wireshark
+brew install zsh
+
+# Remove outdated versions from the cellar
+brew cleanup
 
 exit 0
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,90 +1,104 @@
-autoload colors && colors
-# cheers, @ehrenmurdick
-# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
+autoload -U colors && colors
 
-if (( $+commands[git] ))
-then
-  git="$commands[git]"
-else
-  git="/usr/bin/git"
+function update_current_git_vars() {
+  unset __CURRENT_GIT_BRANCH
+  unset __CURRENT_GIT_BRANCH_STATUS
+  unset __CURRENT_GIT_BRANCH_AHEAD
+  unset __CURRENT_GIT_BRANCH_IS_DIRTY
+  local st="$(git status 2>/dev/null)"
+  if [ -n "$st" ]; then
+    local -a arr
+    arr=(${(f)st})
+    if echo $arr[1] | grep "Not currently on any branch." >/dev/null; then
+      __CURRENT_GIT_BRANCH='no-branch'
+    else
+      __CURRENT_GIT_BRANCH="$(echo $arr[1] | awk ' { print $4 } ')"
+    fi
+    if echo $arr[2] | grep "Your branch is" >/dev/null; then
+      if echo $arr[2] | grep "ahead" >/dev/null; then
+        __CURRENT_GIT_BRANCH_STATUS='ahead'
+        __CURRENT_GIT_BRANCH_AHEAD=$(echo $st | grep 'ahead' | cut -d' ' -f9)
+      elif echo $arr[2] | grep "diverged" >/dev/null; then
+        __CURRENT_GIT_BRANCH_STATUS='diverged'
+      else
+        __CURRENT_GIT_BRANCH_STATUS='behind'
+      fi
+    fi
+    if echo $st | grep "nothing to commit (working directory clean)" >/dev/null; then
+    else
+      __CURRENT_GIT_BRANCH_IS_DIRTY='1'
+    fi
+  fi
+}
+
+function prompt_git_info() {
+  update_current_git_vars
+
+  if [ -n "$__CURRENT_GIT_BRANCH" ]; then
+    local s="${ZSH_THEME_COLOR_YELLOW}${ZSH_THEME_GIT_PROMPT_PREFIX}"
+
+    s+="$__CURRENT_GIT_BRANCH"
+
+    if [ -n "$__CURRENT_GIT_BRANCH_IS_DIRTY" ]; then
+      s+="${ZSH_THEME_COLOR_RED}${ZSH_THEME_GIT_PROMPT_DIRTY}"
+    fi
+
+    case "$__CURRENT_GIT_BRANCH_STATUS" in
+      ahead) s+="${ZSH_THEME_COLOR_GREEN}${ZSH_THEME_GIT_AHEAD}${__CURRENT_GIT_BRANCH_AHEAD}" ;;
+      diverged) s+="${ZSH_THEME_COLOR_GREEN}${ZSH_THEME_GIT_DIVERGED}" ;;
+      behind) s+="${ZSH_THEME_COLOR_GREEN}${ZSH_THEME_GIT_BEHIND}" ;;
+    esac
+
+    s+="${ZSH_THEME_GIT_PROMPT_SUFFIX}${ZSH_THEME_COLOR_RESET}"
+
+    echo ${s}
+  fi
+}
+
+function chpwd_update_git_vars() {
+  update_current_git_vars
+}
+
+function precmd_update_git_vars() {
+  if [ -n "$__EXECUTED_GIT_COMMAND" ]; then
+    update_current_git_vars
+    unset __EXECUTED_GIT_COMMAND
+  fi
+}
+
+function preexec_update_git_vars() {
+  case "$1" in
+    g*|git*|gitx*|gb*|gc*|commit*|vi*|rm*) __EXECUTED_GIT_COMMAND=1 ;;
+  esac
+}
+
+if [ -e ~/.rvm/bin/rvm-prompt ]; then
+  RUBY_PROMPT_="[\$(~/.rvm/bin/rvm-prompt v g s)]"
 fi
 
-git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
+ZSH_THEME_COLOR_YELLOW="%{$fg[yellow]%}"
+ZSH_THEME_COLOR_GREEN="%{$fg[green]%}"
+ZSH_THEME_COLOR_RED="%{$fg[red]%}"
+ZSH_THEME_COLOR_RESET="%{$reset_color%}"
 
-git_dirty() {
-  st=$($git status 2>/dev/null | tail -n 1)
-  if [[ $st == "" ]]
-  then
-    echo ""
+ZSH_THEME_GIT_PROMPT_PREFIX=""
+ZSH_THEME_GIT_PROMPT_SUFFIX=""
+
+ZSH_THEME_GIT_PROMPT_DIRTY="⚡"
+ZSH_THEME_GIT_AHEAD="↑"
+ZSH_THEME_GIT_DIVERGED="↕"
+ZSH_THEME_GIT_BEHIND="↓"
+
+user_at_host() {
+  if [ $UID -eq 0 ]; then
+    echo -n "${ZSH_THEME_COLOR_RED}%n";
   else
-    if [[ "$st" =~ ^nothing ]]
-    then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
-    fi
+    echo -n "${ZSH_THEME_COLOR_GREEN}%n";
   fi
-}
-
-git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- echo "${ref#refs/heads/}"
-}
-
-unpushed () {
-  $git cherry -v @{upstream} 2>/dev/null
-}
-
-need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
-    echo " "
-  else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
+  if [ "$(uname -s)" != "Darwin" ]; then
+    echo -n "%{$fg[green]%}@%m"
   fi
+  echo -n "%{$reset_color%}"
 }
 
-rb_prompt(){
-  if (( $+commands[rbenv] ))
-  then
-	  echo "%{$fg_bold[yellow]%}$(rbenv version | awk '{print $1}')%{$reset_color%}"
-	else
-	  echo ""
-  fi
-}
-
-# This keeps the number of todos always available the right hand side of my
-# command line. I filter it to only count those tagged as "+next", so it's more
-# of a motivation to clear out the list.
-todo(){
-  if (( $+commands[todo.sh] ))
-  then
-    num=$(echo $(todo.sh ls +next | wc -l))
-    let todos=num-2
-    if [ $todos != 0 ]
-    then
-      echo "$todos"
-    else
-      echo ""
-    fi
-  else
-    echo ""
-  fi
-}
-
-directory_name(){
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
-}
-
-export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
-}
-
-precmd() {
-  title "zsh" "%m" "%55<...<%~"
-  set_prompt
-}
+PROMPT='$(user_at_host) %~ $(prompt_git_info) ${ZSH_THEME_COLOR_RED}»${ZSH_THEME_COLOR_RESET} '
