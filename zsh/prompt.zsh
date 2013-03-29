f5768391fd47a62e426ec3fe7541aa05a2e58578
Neal <neal@ineal.me>
2013-03-29 00:20:22 -0500

    made prompt even prettier!

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,99 +1,73 @@
-autoload -U colors && colors
+setopt prompt_subst
+autoload -U colors && colors # Enable colors in prompt
 
-function update_current_git_vars() {
-  unset __CURRENT_GIT_BRANCH
-  unset __CURRENT_GIT_BRANCH_STATUS
-  unset __CURRENT_GIT_BRANCH_AHEAD
-  unset __CURRENT_GIT_BRANCH_IS_DIRTY
-  local st="$(git status 2>/dev/null)"
-  if [ -n "$st" ]; then
-    local -a arr
-    arr=(${(f)st})
-    if echo $arr[1] | grep "Not currently on any branch." >/dev/null; then
-      __CURRENT_GIT_BRANCH='no-branch'
-    else
-      __CURRENT_GIT_BRANCH="$(echo $arr[1] | awk ' { print $4 } ')"
-    fi
-    if echo $arr[2] | grep "Your branch is" >/dev/null; then
-      if echo $arr[2] | grep "ahead" >/dev/null; then
-        __CURRENT_GIT_BRANCH_STATUS='ahead'
-        __CURRENT_GIT_BRANCH_AHEAD=$(echo $st | grep 'ahead' | cut -d' ' -f9)
-      elif echo $arr[2] | grep "diverged" >/dev/null; then
-        __CURRENT_GIT_BRANCH_STATUS='diverged'
-      else
-        __CURRENT_GIT_BRANCH_STATUS='behind'
-      fi
-    fi
-    if echo $st | grep "nothing to commit (working directory clean)" >/dev/null; then
-    else
-      __CURRENT_GIT_BRANCH_IS_DIRTY='1'
-    fi
+# Modify the colors and symbols in these variables as desired.
+GIT_PROMPT_SYMBOL="%{$fg[blue]%}±"
+GIT_PROMPT_PREFIX="%{$fg[green]%}[%{$reset_color%}"
+GIT_PROMPT_SUFFIX="%{$fg[green]%}]%{$reset_color%}"
+GIT_PROMPT_AHEAD="%{$fg[red]%}ANUM%{$reset_color%}"
+GIT_PROMPT_BEHIND="%{$fg[cyan]%}BNUM%{$reset_color%}"
+GIT_PROMPT_MERGING="%{$fg_bold[magenta]%}⚡︎%{$reset_color%}"
+GIT_PROMPT_UNTRACKED="%{$fg_bold[red]%}●%{$reset_color%}"
+GIT_PROMPT_MODIFIED="%{$fg_bold[yellow]%}●%{$reset_color%}"
+GIT_PROMPT_STAGED="%{$fg_bold[green]%}●%{$reset_color%}"
+
+# Show Git branch/tag, or name-rev if on detached head
+parse_git_branch() {
+  (git symbolic-ref -q HEAD || git name-rev --name-only --no-undefined --always HEAD) 2> /dev/null
+}
+
+# Show different symbols as appropriate for various Git repository states
+parse_git_state() {
+
+  # Compose this value via multiple conditional appends.
+  local GIT_STATE=""
+
+  local NUM_AHEAD="$(git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
+  if [ "$NUM_AHEAD" -gt 0 ]; then
+    GIT_STATE=$GIT_STATE${GIT_PROMPT_AHEAD//NUM/$NUM_AHEAD}
   fi
-}
 
-function prompt_git_info() {
-  update_current_git_vars
-
-  if [ -n "$__CURRENT_GIT_BRANCH" ]; then
-    local s="${ZSH_THEME_COLOR_YELLOW}${ZSH_THEME_GIT_PROMPT_PREFIX}"
-
-    s+="$__CURRENT_GIT_BRANCH"
-
-    if [ -n "$__CURRENT_GIT_BRANCH_IS_DIRTY" ]; then
-      s+="${ZSH_THEME_COLOR_RED}${ZSH_THEME_GIT_PROMPT_DIRTY}"
-    fi
-
-    case "$__CURRENT_GIT_BRANCH_STATUS" in
-      ahead) s+="${ZSH_THEME_COLOR_GREEN}${ZSH_THEME_GIT_AHEAD}${__CURRENT_GIT_BRANCH_AHEAD}" ;;
-      diverged) s+="${ZSH_THEME_COLOR_GREEN}${ZSH_THEME_GIT_DIVERGED}" ;;
-      behind) s+="${ZSH_THEME_COLOR_GREEN}${ZSH_THEME_GIT_BEHIND}" ;;
-    esac
-
-    s+="${ZSH_THEME_GIT_PROMPT_SUFFIX}${ZSH_THEME_COLOR_RESET}"
-
-    echo ${s}
+  local NUM_BEHIND="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')"
+  if [ "$NUM_BEHIND" -gt 0 ]; then
+    GIT_STATE=$GIT_STATE${GIT_PROMPT_BEHIND//NUM/$NUM_BEHIND}
   fi
-}
 
-function chpwd_update_git_vars() {
-  update_current_git_vars
-}
-
-function precmd_update_git_vars() {
-  if [ -n "$__EXECUTED_GIT_COMMAND" ]; then
-    update_current_git_vars
-    unset __EXECUTED_GIT_COMMAND
+  local GIT_DIR="$(git rev-parse --git-dir 2> /dev/null)"
+  if [ -n $GIT_DIR ] && test -r $GIT_DIR/MERGE_HEAD; then
+    GIT_STATE=$GIT_STATE$GIT_PROMPT_MERGING
   fi
+
+  if [[ -n $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then
+    GIT_STATE=$GIT_STATE$GIT_PROMPT_UNTRACKED
+  fi
+
+  if ! git diff --quiet 2> /dev/null; then
+    GIT_STATE=$GIT_STATE$GIT_PROMPT_MODIFIED
+  fi
+
+  if ! git diff --cached --quiet 2> /dev/null; then
+    GIT_STATE=$GIT_STATE$GIT_PROMPT_STAGED
+  fi
+
+  if [[ -n $GIT_STATE ]]; then
+    echo "$GIT_PROMPT_PREFIX$GIT_STATE$GIT_PROMPT_SUFFIX"
+  fi
+
 }
 
-function preexec_update_git_vars() {
-  case "$1" in
-    g*|git*|gitx*|gb*|gc*|commit*|vi*|rm*) __EXECUTED_GIT_COMMAND=1 ;;
-  esac
+# If inside a Git repository, print its branch and state
+git_prompt_string() {
+  local git_where="$(parse_git_branch)"
+  [ -n "$git_where" ] && echo "$GIT_PROMPT_SYMBOL$(parse_git_state)$GIT_PROMPT_PREFIX%{$fg[yellow]%}${git_where#(refs/heads/|tags/)}$GIT_PROMPT_SUFFIX"
 }
 
-if [ -e ~/.rvm/bin/rvm-prompt ]; then
-  RUBY_PROMPT_="[\$(~/.rvm/bin/rvm-prompt v g s)]"
-fi
-
-ZSH_THEME_COLOR_YELLOW="%{$fg[yellow]%}"
-ZSH_THEME_COLOR_GREEN="%{$fg[green]%}"
-ZSH_THEME_COLOR_RED="%{$fg[red]%}"
-ZSH_THEME_COLOR_RESET="%{$reset_color%}"
-
-ZSH_THEME_GIT_PROMPT_PREFIX=""
-ZSH_THEME_GIT_PROMPT_SUFFIX=""
-
-ZSH_THEME_GIT_PROMPT_DIRTY="⚡"
-ZSH_THEME_GIT_AHEAD="↑"
-ZSH_THEME_GIT_DIVERGED="↕"
-ZSH_THEME_GIT_BEHIND="↓"
-
+# Get the user@hostname with colors depending on UID and hostname if not Darwin
 user_at_host() {
   if [ $UID -eq 0 ]; then
-    echo -n "${ZSH_THEME_COLOR_RED}%n";
+    echo -n "%{$fg[magenta]%}%n";
   else
-    echo -n "${ZSH_THEME_COLOR_GREEN}%n";
+    echo -n "%{$fg[green]%}%n";
   fi
   if [ "$(uname -s)" != "Darwin" ]; then
     echo -n "%{$fg[green]%}@%m"
@@ -101,4 +75,7 @@ user_at_host() {
   echo -n "%{$reset_color%}"
 }
 
-PROMPT='$(user_at_host) %~ $(prompt_git_info) ${ZSH_THEME_COLOR_RED}»${ZSH_THEME_COLOR_RESET} '
+# Set the right-hand prompt
+RPS1='$(git_prompt_string)'
+
+PROMPT='$(user_at_host) %~ %{$fg[red]%}»%{$reset_color%} '
