ebf285faf6f0ef5965199dc1dd2b2e2ea6b200ab
Neal <neal@ineal.me>
2013-03-29 01:48:24 -0500

    too used to la

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -6,7 +6,7 @@ then
   alias ls="gls -F --color"
   alias l="gls -lAh --color"
   alias ll="gls -l --color"
-  alias la='gls -A --color'
+  alias la='gls -lA --color'
 fi
 
 # Push and pop directories on directory stack
