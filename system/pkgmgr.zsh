6318edfea11ebe93e12e96adc1041a4db7bede31
Neal <neal@ineal.me>
2013-04-29 01:34:37 -0500

    changed pkgmgr aliases as 'update' on linux was conflicting with other things

--- system/pkgmgr.zsh
+++ system/pkgmgr.zsh
@@ -1,16 +1,16 @@
 if [[ "$(uname -s)" == "Linux" ]]; then
 
-	alias update="sudo apt-get -qq update && sudo apt-get upgrade && sudo apt-get dist-upgrade"
-	alias install="sudo apt-get install"
-	alias remove="sudo apt-get remove"
-	alias search="apt-cache search"
+	alias apt-update="sudo apt-get -qq update && sudo apt-get upgrade && sudo apt-get dist-upgrade"
+	alias apt-install="sudo apt-get install"
+	alias apt-remove="sudo apt-get remove"
+	alias apt-search="apt-cache search"
 
 elif [[ "$(uname -s)" == "Darwin" ]]; then
 
-	alias update="brew update && brew upgrade"
-	alias install="brew install"
-	alias uninstall="brew uninstall"
-	alias remove="$uninstall"
+	alias brew-update="brew update && brew upgrade"
+	alias brew-install="brew install"
+	alias brew-uninstall="brew uninstall"
+	alias brew-remove="$uninstall"
 
 	alias supdate="sudo softwareupdate -ia"
 fi
