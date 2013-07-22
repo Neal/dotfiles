1e5409eb2b876b87dcb75a150cef37c6b3acf838
Neal <neal@ineal.me>
2013-07-22 10:27:53 -0500

    Add tmux oh-my-zsh plugin.

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -24,7 +24,7 @@ ZSH_THEME="lust"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(battery colored-man colorize cp extract history git gitfast github git-extras lol rsync safe-paste screen)
+plugins=(battery colored-man colorize cp extract history git gitfast github git-extras lol rsync safe-paste screen tmux)
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
 	plugins=(${plugins[@]} brew copydir copyfile osx sublime terminalapp)
