cd82c47e800c6ff3b6412a790e4c1367151a9ee1
Neal <neal@ineal.me>
2013-10-03 01:20:21 -0500

    Add tmuxinator plugin.

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -24,7 +24,7 @@ ZSH_THEME="lust"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(battery colored-man colorize cp extract history git gitfast github git-extras lol rsync safe-paste screen tmux)
+plugins=(battery colored-man colorize cp extract history git gitfast github git-extras lol rsync safe-paste screen tmux tmuxinator)
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
 	plugins=(${plugins[@]} brew copydir copyfile osx sublime terminalapp)
