9b20e4788341bfb12267c6de465b2ecf2aecae44
Neal <neal@ineal.me>
2013-10-30 12:34:50 -0500

    Fix typo!

--- osx/path.zsh
+++ osx/path.zsh
@@ -1,7 +1,7 @@
 # Set custom OS X PATH
 if [[ "$(uname -s)" == "Darwin" ]]; then
 	# Pebble SDK
-	export PATH=$(HOME)/pebble-dev/arm-cs-tools/bin:$PATH
+	export PATH=$HOME/pebble-dev/arm-cs-tools/bin:$PATH
 	# Coreutils
 	export PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
 fi
