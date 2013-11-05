8a225a052f801f18ff566e95c8594e52efa10ad3
Neal <neal@ineal.me>
2013-11-05 04:46:51 -0600

    Whoops. Forgot to change everything.

--- osx/path.zsh
+++ osx/path.zsh
@@ -1,7 +1,8 @@
 # Set custom OS X PATH. Abort if not OS X.
 [[ "$OSTYPE" =~ ^darwin ]] || return 1
-	# Pebble SDK
-	export PATH=$HOME/pebble-dev/arm-cs-tools/bin:$PATH
-	# Coreutils
-	export PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
-fi
+
+# Pebble SDK
+export PATH=$HOME/pebble-dev/arm-cs-tools/bin:$PATH
+
+# Coreutils
+export PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
