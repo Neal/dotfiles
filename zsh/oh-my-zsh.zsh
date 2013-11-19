f3cf359e6f6aa553200b6d3d20482d4ee248fd19
Neal <neal@ineal.me>
2013-11-18 21:17:40 -0600

    Update oh-my-zsh plugins.

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -4,10 +4,10 @@ ZSH_THEME="lust"
 
 DISABLE_AUTO_UPDATE="true"
 
-plugins=(autoenv battery colored-man cp extract history gem git gitfast gitignore git-extras lol mosh npm python rsync safe-paste screen tmux tmuxinator vundle)
+plugins=(autoenv battery colored-man command-not-found cp encode64 extract history gem git gitfast gitignore git-extras lol mosh npm python rsync safe-paste screen tmux tmuxinator urltools vundle)
 
-if [[ "$(uname -s)" == "Darwin" ]]; then
-	plugins=(${plugins[@]} brew copydir copyfile osx sublime)
+if [[ "$OSTYPE" =~ ^darwin ]]; then
+	plugins=(${plugins[@]} brew cloudapp copydir copyfile osx sublime)
 elif [ -f /etc/debian_version ]; then
 	plugins=(${plugins[@]} debian)
 fi
