b2e5d1cf46f9511b52503f83cf090364b1435cd1
Neal <neal@ineal.me>
2013-11-18 21:19:24 -0600

    Move Pebble SDK bin to ~/.localrc

--- osx/path.zsh
+++ osx/path.zsh
@@ -1,8 +1,5 @@
 # Set custom OS X PATH. Abort if not OS X.
 [[ "$OSTYPE" =~ ^darwin ]] || return 1
 
-# Pebble SDK
-export PATH=$HOME/pebble-dev/arm-cs-tools/bin:$PATH
-
 # Coreutils
 export PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
