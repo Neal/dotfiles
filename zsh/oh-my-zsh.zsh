8faf989c5d3efc1919073b96108ee954623122a1
Neal <neal@ineal.me>
2014-04-10 17:54:04 -0500

    Add brew-cask oh-my-zsh plugin.

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -9,7 +9,7 @@ DISABLE_AUTO_UPDATE="true"
 plugins=(autoenv battery colored-man command-not-found cp encode64 extract history gem git gitfast gitignore git-extras lol mosh npm python rsync safe-paste screen systemadmin tmux tmuxinator urltools vundle)
 
 if [[ "$OSTYPE" =~ ^darwin ]]; then
-	plugins=(${plugins[@]} brew cloudapp copydir copyfile osx sublime)
+	plugins=(${plugins[@]} brew brew-cask cloudapp copydir copyfile osx sublime)
 elif [ -f /etc/debian_version ]; then
 	plugins=(${plugins[@]} debian)
 fi
