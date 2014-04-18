5cf2f97c76f77d46e5e09aa3274545226dddd1bc
Neal <neal@ineal.me>
2014-04-17 21:41:53 -0500

    Update rbenv stuff to latest

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -3,13 +3,14 @@ rbenv rehash 2>/dev/null
 
 # shell thing
 rbenv() {
+  typeset command
   command="$1"
   if [ "$#" -gt 0 ]; then
     shift
   fi
 
   case "$command" in
-  shell)
+  rehash|shell)
     eval `rbenv "sh-$command" "$@"`;;
   *)
     command rbenv "$command" "$@";;
