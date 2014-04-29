bca5b8860f6c8f1166e5c314e643efdbe7e611ab
Neal <neal@ineal.me>
2014-04-29 11:06:49 -0500

    Add pip oh-my-zsh plugin

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -6,7 +6,7 @@ ZSH_THEME="lust"
 
 DISABLE_AUTO_UPDATE="true"
 
-plugins=(autoenv battery colored-man command-not-found cp encode64 extract history gem git gitfast gitignore git-extras lol mosh npm python rsync safe-paste screen systemadmin tmux tmuxinator urltools vundle)
+plugins=(autoenv battery colored-man command-not-found cp encode64 extract history gem git gitfast gitignore git-extras lol mosh npm pip python rsync safe-paste screen systemadmin tmux tmuxinator urltools vundle)
 
 if [[ "$OSTYPE" =~ ^darwin ]]; then
 	plugins=(${plugins[@]} brew brew-cask cloudapp copydir copyfile osx sublime)
