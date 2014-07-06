95a3ec565c06762c7aaef06315b36870d8b165d9
Neal <neal@ineal.me>
2014-07-06 10:02:00 -0500

    Add ~/bin to PATH.

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/opt/ruby/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$DOTFILES/bin:$PATH"
+export PATH="./bin:$HOME/bin:$HOME/.rbenv/shims:/usr/local/opt/ruby/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$DOTFILES/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
