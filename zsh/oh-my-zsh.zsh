4c50aeafa3e1051e8c8429baadb80ba71f832f85
Neal <neal@ineal.me>
2014-10-13 17:29:30 -0500

    Add pure theme and zsh-syntax-highliting.

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -2,11 +2,11 @@ ZSH=$HOME/.oh-my-zsh
 
 ZSH_CUSTOM=$HOME/.dotfiles/oh-my-zsh
 
-ZSH_THEME="lust"
+ZSH_THEME="pure"
 
 DISABLE_AUTO_UPDATE="true"
 
-plugins=(autoenv battery colored-man command-not-found cp encode64 extract history gem git gitfast gitignore git-extras mosh npm pip python rsync safe-paste screen sudo systemadmin tmux tmuxinator urltools virtualenv vundle wd zsh_reload)
+plugins=(autoenv battery colored-man command-not-found cp encode64 extract heroku history history-substring-search gem git gitfast gitignore git-extras mosh npm pip python rsync safe-paste screen sudo systemadmin tmux tmuxinator urltools virtualenv vundle wd zsh_reload zsh-syntax-highlighting)
 
 if [[ "$OSTYPE" =~ ^darwin ]]; then
 	plugins=(${plugins[@]} brew brew-cask cloudapp copydir copyfile osx sublime vagrant xcode)
