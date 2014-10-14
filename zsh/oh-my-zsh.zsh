9a94050aa63ea366cd4d5e8a2d8c704a1ec3a727
Neal <neal@ineal.me>
2014-10-14 13:16:42 -0500

    Split oh-my-zsh packages in new lines and add golang and ssh-agent.

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -6,10 +6,59 @@ ZSH_THEME="pure"
 
 DISABLE_AUTO_UPDATE="true"
 
-plugins=(autoenv battery colored-man command-not-found cp encode64 extract heroku history history-substring-search gem git gitfast gitignore git-extras mosh npm pip python rsync safe-paste screen sudo systemadmin tmux tmuxinator urltools virtualenv vundle wd zsh_reload zsh-syntax-highlighting)
+plugins=(
+	autoenv
+	battery
+	colored-man
+	command-not-found
+	cp encode64
+	extract
+	heroku
+	history-substring-search
+	history
+	golang
+	gem
+	git
+	gitfast
+	gitignore
+	git-extras
+	iwhois
+	mosh
+	npm
+	pip
+	python
+	rsync
+	safe-paste
+	screen
+	sudo
+	ssh-agent
+	systemadmin
+	tmux
+	tmuxinator
+	urltools
+	virtualenv
+	vundle
+	wd
+	zsh_reload
+	zsh-syntax-highlighting
+)
 
 if [[ "$OSTYPE" =~ ^darwin ]]; then
-	plugins=(${plugins[@]} brew brew-cask cloudapp copydir copyfile osx sublime vagrant xcode)
+	plugins=(
+		${plugins[@]}
+		brew
+		brew-cask
+		cloudapp
+		copydir
+		copyfile
+		osx
+		sublime
+		vagrant
+		xcode
+	)
 elif [ -f /etc/debian_version ]; then
-	plugins=(${plugins[@]} debian)
+	plugins=(
+		${plugins[@]}
+		debian
+	)
 fi
