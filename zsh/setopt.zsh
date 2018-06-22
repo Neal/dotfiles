a7273bb9cef579241fff97bdaf538292996ceadf
Neal Patel <neal@ineal.me>
2018-06-22 15:20:49 -0700

    Disable zsh complete_aliases

--- zsh/setopt.zsh
+++ zsh/setopt.zsh
@@ -29,7 +29,7 @@ setopt always_to_end # When completing from the middle of a word, move the curso
 setopt auto_menu # show completion menu on successive tab press. needs unsetop menu_complete to work
 setopt auto_name_dirs # any parameter that is set to the absolute name of a directory immediately becomes a name for that directory
 setopt complete_in_word # if unset the cursor is set to the end of the word if completion is started
-setopt complete_aliases # don't expand aliases _before_ completion has finished
+# setopt complete_aliases # don't expand aliases _before_ completion has finished
 
 unsetopt menu_complete # do not autoselect the first completion entry
 
